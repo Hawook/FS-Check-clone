5483c266c73f51e13f2c9d9529a7c305d5e56b7b
Hawook <Hawook@users.noreply.github.com>
2020-02-26 19:25:05 +0500

    Немного обработал
    Перенес структуру с инициализацией переменных в объекте с типами данных для вычисления параметров ФС

--- FSDll/FSDLL.h
+++ FSDll/FSDLL.h
@@ -2,3 +2,32 @@
 #include <tchar.h>
 #endif
 
+using namespace std;
+
+typedef struct
+{
+	BYTE jumpCode[3];
+	BYTE oemID[8];
+	WORD bytesPerSector;
+	BYTE sectorPerCluster;
+	WORD reservedSectors;
+	BYTE always0[3];
+	WORD notUsed;
+	BYTE mediaDiscr;
+	WORD always02;
+	WORD sectorperTrack;
+	WORD numOfHeaders;
+	DWORD hiddenSectors;
+	DWORD notUsed2;
+	DWORD notused3;
+	ULONGLONG totalSectors;
+	ULONGLONG lcnForSMFT;
+	ULONGLONG lcnForSMFTMirr;
+	DWORD clusterPerFileRecordSegm;
+	BYTE clusterPerIndexBuff;
+	BYTE notUsed4[3];
+	ULONGLONG volumeSerialNumber;
+	DWORD checksum;
+	BYTE bootstrap[426];
+	WORD endMarker;
+} NTFS_BootRecord;
